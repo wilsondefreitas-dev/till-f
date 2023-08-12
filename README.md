c467e3648b9e3099d049fad6e6f81936f78cc7e1
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-08-12 03:50:05 -0300

    setup prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Till-F
+
 Till-F is a system to manage workout plan, to record data and measure progress.
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,3 @@
 export default function Home() {
-
-  return (
-
-    <h1>🚧 Staging também em construção...</h1>
-
-  );
-
+  return <h1>🚧 Staging também em construção...</h1>;
 }
