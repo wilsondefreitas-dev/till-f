3ff01ca151642d5f91dd2cb2da9da5c12e6cc126
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-08-13 22:39:36 -0300

    add console.log warning rule on eslint

---
+++ app/(routes)/feed/page.jsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Fab from "@mui/material/Fab";
+import Chip from "@mui/material/Chip";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import AddIcon from "@mui/icons-material/Add";
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
+import TrainingCard from "./TrainingCard";
+
+export default function Feed() {
+  const feedContainer = useRef(null);
+  const buttonToTrain = useRef(null);
+  const fabToTrain = useRef(null);
+
+  useEffect(() => {
+    if (typeof window === "object" && typeof document === "object") {
+      showFabOnScroll();
+    }
+  }, []);
+
+  function showFabOnScroll() {
+    const feedPaddingTopSize = parseInt(
+      window
+        .getComputedStyle(feedContainer.current)
+        .getPropertyValue("padding-top"),
+    );
+
+    const buttonHeight = buttonToTrain.current.offsetHeight;
+
+    (document.onscroll = () => {
+      const shoFab = window.scrollY >= feedPaddingTopSize + buttonHeight;
+      fabToTrain.current.style.opacity = Number(shoFab);
+    })();
+  }
+
+  return (
+    <FeedContainer ref={feedContainer} maxWidth="sm">
+      <LongButton ref={buttonToTrain} variant="contained">
+        Novo Treino
+      </LongButton>
+      <FixedFab ref={fabToTrain} color="primary" aria-label="add">
+        <AddIcon />
+      </FixedFab>
+      <Divider>
+        <Chip size="small" label="HISTÓRICO DE TREINOS" />
+      </Divider>
+      <HistoricContainer>
+        <TrainingCard />
+        <TrainingCard />
+        <TrainingCard />
+        <TrainingCard />
+        <TrainingCard />
+      </HistoricContainer>
+    </FeedContainer>
+  );
+}
+
+const FeedContainer = styled(Container)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: 18,
+  paddingTop: 24,
+  paddingBottom: 24,
+  minHeight: "100vh",
+  backgroundColor: "gainsboro",
+  boxShadow: "0px 0px 20px -1px rgba(0, 0, 0, 0.4)",
+}));
+
+const FixedFab = styled(Fab)(() => ({
+  position: "fixed",
+  bottom: 24,
+  right: 24,
+  opacity: 0,
+  transition: "opacity 0.25s",
+}));
+
+const LongButton = styled(Button)(() => ({
+  width: "100%",
+  height: 50,
+}));
+
+const HistoricContainer = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: 10,
+  width: "100",
+}));
