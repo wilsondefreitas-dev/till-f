4700200ba74a6a9db7a6f13f328014e03473b0a8
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-08-15 00:43:40 -0300

    move container to layout

--- app/(routes)/feed/page.jsx
+++ app/(routes)/feed/page.jsx
@@ -1,14 +1,12 @@
 "use client";
-
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import TrainingCard from "./components/TrainingCard";
 import NewTrainingButton from "./components/NewTrainingButton";
 
 export default function Feed() {
   return (
-    <FeedContainer maxWidth="sm">
+    <>
       <NewTrainingButton />
       <FeedHeader>histórico</FeedHeader>
 
@@ -17,19 +15,9 @@ export default function Feed() {
           <TrainingCard key={data} />
         ))}
       </HistoricContainer>
-    </FeedContainer>
+    </>
   );
 }
-
-const FeedContainer = styled(Container)(() => ({
-  display: "flex",
-  flexDirection: "column",
-  gap: 18,
-  paddingBottom: 24,
-  minHeight: "100vh",
-  backgroundColor: "gainsboro",
-  boxShadow: "0px 0px 20px -1px rgba(0, 0, 0, 0.4)",
-}));
 
 const HistoricContainer = styled("div")(() => ({
   display: "flex",
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,11 @@
+"use client";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./_styles/global.css";
+import styled from "@emotion/styled";
+import Container from "@mui/material/Container";
 
 export const metadata = {
   title: "Till-F",
@@ -12,7 +15,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Maincontainer maxWidth="sm">{children}</Maincontainer>
+      </body>
     </html>
   );
 }
+
+const Maincontainer = styled(Container)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: 18,
+  paddingBottom: 24,
+  minHeight: "100vh",
+  backgroundColor: "gainsboro",
+  boxShadow: "0px 0px 20px -1px rgba(0, 0, 0, 0.4)",
+}));
